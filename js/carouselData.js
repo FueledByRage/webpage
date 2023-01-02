6dc65a5b7ae48a879ab27fb2172dd13210df5bd2
FueledByRage <erkmgwr@gmail.com>
2023-01-02 14:31:39 -0300

    Responsivity and fixing link on images

--- js/carouselData.js
+++ js/carouselData.js
@@ -1,25 +1,25 @@
 export const carouselData = [
     {
         title: '',
-        previewUrl: '../assets/images/waystoneInn.png',
+        previewUrl: './assets/images/waystoneInn.png',
         about: 'Um forum estilo Reddit que tenho trabalhado há um tempo, serve principalmente como meu laboratorio de experiência para aprendizados.',
         githubUrl: 'https://github.com/FueledByRage/The-Waystone-Inn-Frontend'
     },
     {
         title: '',
-        previewUrl: '../assets/images/clips.png',
+        previewUrl: './assets/images/clips.png',
         about: 'A primeira ideia era trabalhar com NextJS fazendo uma plataforma de mini posts, estilo twitter. Evolui para um site de vídeos e adcionei uma API com PHP vanilla e SOLID como desafio. ',
         githubUrl: 'https://github.com/FueledByRage/front-end-php-next'
     },
     {
         title: '',
-        previewUrl: '../assets/images/discordBot.png',
+        previewUrl: './assets/images/discordBot.png',
         about: 'Queria colocar a mão na massa em desenvolvimento para bots do discord, então tive a ideia de um bot que auxilia com atividades que eu fazia em meu dia a dia como desenvolvedor.',
         githubUrl: 'https://github.com/FueledByRage/andros'
     },
     {
         title: '',
-        previewUrl: '../assets/images/resumes.png',
+        previewUrl: './assets/images/resumes.png',
         about: 'Organizador de currículos que eu fiz para um desafio utilizando Laravel Lumen. Achei a ferramenta extremamente util, como já possuia conhecimento em vários conceitos web que o framework utiliza foi um aprendizado tranquilo.',
         githubUrl: 'https://github.com/FueledByRage/Codificar-Organizador-De-Curriculos'
     },
