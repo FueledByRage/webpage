fa90c36b09da114f3a4a9ea9e52095ab0af85a59
FueledByRage <erkmgwr@gmail.com>
2023-01-05 10:45:15 -0300

    Changing the language

--- js/carouselData.js
+++ js/carouselData.js
@@ -2,31 +2,31 @@ export const carouselData = [
     {
         title: '',
         previewUrl: './assets/images/waystoneInn.png',
-        about: 'Um forum estilo Reddit que tenho trabalhado há um tempo, serve principalmente como meu laboratorio de experiência para aprendizados.',
+        about: 'A Reddit like forum that i have been working for a while. It is usually my lab to improve and i have learned a lot with it ',
         githubUrl: 'https://github.com/FueledByRage/The-Waystone-Inn-Frontend'
     },
     {
         title: '',
         previewUrl: './assets/images/clips.png',
-        about: 'A primeira ideia era trabalhar com NextJS fazendo uma plataforma de mini posts, estilo twitter. Evolui para um site de vídeos e adcionei uma API com PHP vanilla e SOLID como desafio. ',
+        about: 'The original idea was to be a micro blog, just like twitter. Using NextJs just because i was, and still, hyping SSR. The backend is written in vanilla PHP, as a personal challenge (I suffered since the very beginning)',
         githubUrl: 'https://github.com/FueledByRage/front-end-php-next'
     },
     {
         title: '',
         previewUrl: './assets/images/discordBot.png',
-        about: 'Queria colocar a mão na massa em desenvolvimento para bots do discord, então tive a ideia de um bot que auxilia com atividades que eu fazia em meu dia a dia como desenvolvedor.',
+        about: 'My goal was to make a automization tool to generate some tiring code, i thought going with a discord bot was i good idea.',
         githubUrl: 'https://github.com/FueledByRage/andros'
     },
     {
         title: '',
         previewUrl: './assets/images/resumes.png',
-        about: 'Organizador de currículos que eu fiz para um desafio utilizando Laravel Lumen. Achei a ferramenta extremamente util, como já possuia conhecimento em vários conceitos web que o framework utiliza foi um aprendizado tranquilo.',
+        about: 'A resumes organizer using Lumen Laravel i developed for a challenge.',
         githubUrl: 'https://github.com/FueledByRage/Codificar-Organizador-De-Curriculos'
     },
     {
         title: '',
         previewUrl: './assets/images/email-sender.png',
-        about: 'Microserviço para envio de emails usando Java e Spring Boot',
+        about: 'Having a microservice to send emails is usefull in many ways so i worked on my own using Spring Boot that is my favourite web framework.',
         githubUrl: 'https://github.com/FueledByRage/microservice-email'
 
     },
